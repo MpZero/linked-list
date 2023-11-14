2a8de1da6b4ad48501fd88861eb50647da1825f7
gonzalo <gonzalo.a.gomez22@gmail.com>
2023-11-14 18:21:55 -0300

    add methods to get list's head and tail. Add method to pop last item off the list

--- linkedList.js
+++ linkedList.js
@@ -3,31 +3,76 @@ class LinkedList {
     this.head = null;
     this.length = 0;
   }
+
   append(value) {
-    let node = new Node(value, this.head);
-    this.head = node;
-    console.log(this.head);
-    console.log(value);
+    if (this.head == null) {
+      this.head = new Node(value);
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = new Node(value);
+    }
     this.length++;
   }
-  prepend(value) {}
+
+  prepend(value) {
+    let current = this.head; //a
+    // console.log(current);
+    if (this.head !== null) {
+      //execute
+      this.head = new Node(value); //a = 3
+    }
+    this.head.next = current; //a = head.next
+    this.length++;
+  }
 
   size() {
     console.log(this.length);
   }
-  // head() {}
-  // tail() {}
-  // at(index) {}
-  // pop() {}
-  // contains(value) {}
-  // find(value) {}
-  // toString() {}
+
+  getHead() {
+    console.log(this.head);
+  }
+
+  getTail() {
+    let current = this.head;
+    if (this.head == null) {
+      return (this.head = null);
+    }
+    while (current.next) {
+      current = current.next;
+    }
+    console.log(current);
+  }
+
+  pop() {
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
+
+// contains(value) {}
+// find(value) {}
+// toString() {}
 
 class Node {
   constructor(value, next) {
     this.value = value;
-    this.next = next;
+    this.next = null;
   }
 }
 
@@ -38,4 +83,31 @@ list.append("C");
 list.append("D");
 list.append("E");
 list.append("F");
-list.size();
+
+// list.prepend(3);
+// list.prepend(2);
+// list.prepend(1);
+
+// list.append("tail");
+// list.prepend("head");
+
+// list.prepend("A");
+// list.prepend("B");
+// list.prepend("C");
+// list.prepend("D");
+// list.prepend("E");
+// list.prepend("F");
+
+// console.log("--------GET_HEAD-----------------");
+// list.getHead();
+// // list.getTail();
+// console.log("--------SIZE-----------------");
+// list.size();
+// console.log("--------POP-----------------");
+// list.pop();
+// console.log("--------SIZE-----------------");
+// list.size();
+// console.log("--------GET_TAIL-----------------");
+// list.getTail();
+console.log("--------LISTA-----------------");
+console.log(list);
